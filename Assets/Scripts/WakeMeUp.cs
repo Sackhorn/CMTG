32fd81ecfe37a1ccd9c0057e627cf6154640bb20
mafiesto4 <mafiesto4.4@gmail.com>
2015-07-24 18:39:16 +0200

    first minigame start

---
+++ Assets/Scripts/WakeMeUp.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WakeMeUp : MonoBehaviour
+{
+    public GameObject Head;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		// center head
+	    var headSprite = Head.GetComponent<SpriteRenderer>();
+	    var headSpriteSize = headSprite.sprite.textureRect;
+        Debug.LogWarning(headSpriteSize);
+        Debug.LogWarning(Screen.width);
+        Head.transform.localScale = new Vector3(Screen.width / headSpriteSize.width, Screen.height / headSpriteSize.height, 1);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+}
