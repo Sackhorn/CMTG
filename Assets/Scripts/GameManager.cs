342a9ec146e2145ed9f3a52388771bde6699520b
Kacper Bak <kbak@outlook.com>
2015-07-25 14:37:32 +0200

    v0.1.2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,49 +1,48 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEditor;
 
 public class GameManager : MonoBehaviour
 {
-	private static GameManager _instance;
+    private static GameManager _instance;
 
-	static public bool isActive
-	{
-		get
-		{
-			return _instance != null;
-		}
-	}
+    static public bool isActive
+    {
+        get
+        {
+            return _instance != null;
+        }
+    }
 
-	public static GameManager Instance
-	{
-		get
-		{
-			if (_instance == null)
-			{
-				_instance = Object.FindObjectOfType(typeof(GameManager)) as GameManager;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = Object.FindObjectOfType(typeof(GameManager)) as GameManager;
 
-				if (_instance == null)
-				{
-					GameObject go = new GameObject("_gameManager");
-					DontDestroyOnLoad(go);
-					_instance = go.AddComponent<GameManager>();
-				}
-			}
-			return _instance;
-		}
-	}
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject("_gameManager");
+                    DontDestroyOnLoad(go);
+                    _instance = go.AddComponent<GameManager>();
+                }
+            }
+            return _instance;
+        }
+    }
 
-	public void StartMiniGame(float seconds)
-	{
-		Object prefab = AssetDatabase.LoadAssetAtPath("Assets/Prefarbs/Timming.prefab", typeof(GameObject));
-		GameObject go = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
-		var timming = go.GetComponent<Timming>();
-		timming.TotalTime = seconds;
-		timming.OnFinish = GameOver;
-	}
+    public void StartMiniGame(float seconds)
+    {
+        //Object prefab = AssetDatabase.LoadAssetAtPath("Assets/Prefarbs/Timming.prefab", typeof(GameObject));
+        Object prefab = Resources.Load("Timming");
+        GameObject go = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
+        var timming = go.GetComponent<Timming>();
+        timming.TotalTime = seconds;
+        timming.OnFinish = GameOver;
+    }
 
-	public void GameOver()
-	{
-		Fade.FadeThisSit("gameKurwaOver", 0.4f);
-	}
+    public void GameOver()
+    {
+        Fade.FadeThisSit("gameKurwaOver", 0.4f);
+    }
 }
