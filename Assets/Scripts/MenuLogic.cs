f4e5f69d814b4e3d009246eaca51540ce6bf835f
mafiesto4 <mafiesto4.4@gmail.com>
2015-07-26 04:49:36 +0200

    menu lepsze

--- Assets/Scripts/MenuLogic.cs
+++ Assets/Scripts/MenuLogic.cs
@@ -3,9 +3,26 @@ using UnityEngine.UI;
 
 public class MenuLogic : MonoBehaviour
 {
+    private Camera _cam;
+    private Vector3 _initPos;
+
     private void Start()
     {
        // GameManager.Instance.StartMiniGame(10.0f, -1, 0, 0);
+
+        _cam = GameObject.Find("Camera").GetComponent<Camera>();
+        _initPos = _cam.transform.position;
+    }
+
+    private void Update()
+    {
+        // Debug.LogWarning(_cam.transform.position);
+
+        //Debug.LogWarning((float)Screen.width / Screen.height);
+
+        //280, 480
+        _cam.transform.position = _initPos - new Vector3(Mathf.Lerp(0, 150, 1 - Mathf.Clamp01((float)Screen.width / Screen.height - 1.0f)), 0, 0);
+        //_cam.transform.position = _initPos - new Vector3(Mathf.Lerp(0, , Screen.width * 0.4f), 0, 0);
     }
 
     public void doIt()
